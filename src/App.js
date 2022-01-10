a759e6419920d26feb23cc92d7d0f4e11b14447b
jongin1004 <bill1224@naver.com>
2022-01-10 23:06:42 +0900

    create component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
-import logo from './logo.svg';
 import './main.css';
+
+function Header() {
+  return (
+    <header>
+      <h1>WEB</h1>
+      <div>React</div>    
+    </header>
+  );
+}
+
+function TOC() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="#">HTML</a></li>
+        <li><a href="#">CSS</a></li>
+        <li><a href="#">JAVASCRIPT</a></li>        
+      </ul>
+    </nav>
+  );
+}
+
+function Content() {
+  return (
+    <article>
+      <div>
+        안녕하세요.
+      </div>
+    </article>
+  );
+}
+
 
 function App() {
   return (
     <div className="App">
-      hello react!!
+      <Header />
+      <TOC />
+      <Content />
     </div>
   );
 }
